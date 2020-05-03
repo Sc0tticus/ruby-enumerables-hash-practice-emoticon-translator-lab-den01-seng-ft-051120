eb78207636d06594ff9c825f915b03822188dec3
Scott Singer <ssinger303@gmail.com>
2020-05-03 03:44:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,47 @@
 # require modules here
 
-def load_library
+#def load_library
   # code goes here
+#end
+
+#def get_japanese_emoticon
+  # code goes here
+#end
+
+#def get_english_meaning
+  # code goes here
+#end
+
+require "yaml"
+require 'pry'
+
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emot_hash = { "get_meaning" => {}, "get_emoticon" => {} }
+  emoticons.each do | emotion, arr |
+   en, jp = arr
+    emot_hash["get_meaning"][jp] = emotion
+    emot_hash["get_emoticon"][en] = jp
+  end
+ emot_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  emot_hash = load_library(path)
+  found = emot_hash["get_emoticon"][emoticon]
+  if found
+   found
+  else 
+   "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  emot_hash = load_library(path)
+  found = emot_hash["get_meaning"][emoticon]
+  if found
+   found 
+  else
+   "Sorry, that emoticon was not found"
+  end
 end
